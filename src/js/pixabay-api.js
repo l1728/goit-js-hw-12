af0572aeb77c32c7979ab66e0d601a8f81d30859
l1728 <cheburashka077@ukr.net>
2024-04-12 23:24:19 +0300

    add_btn

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -36,33 +36,6 @@ export async function fetchImages(query) {
     
     
     
-    
-    
-    
-//   return fetch(url)
-//     .then(response => {
-//       if (!response.ok) {
-//         // Обробляємо випадок невдалого HTTP-запиту
-//         throw new Error("Failed to fetch images");
-//       }
-//       // Повертаємо відповідь у форматі JSON
-//       return response.json();
-//     })
-//     .then(data => {
-//       if (data.hits.length === 0) {
-//         // Якщо немає результатів, повертаємо порожній масив
-//         return [];
-//       }
-//       // Повертаємо масив зображень
-//       return data.hits;
-//     })
-//     .catch(error => {
-//       // Обробляємо помилки
-//       console.error("Error fetching images:", error);
-//       // Повертаємо порожній масив в разі помилки
-//       return [];
-//     });
-// };
 
   
 
